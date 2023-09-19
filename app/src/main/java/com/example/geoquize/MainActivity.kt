674e20deacd8281466bcc1e612a87af8247f5883
Anastasia <aachuchkalova@gmail.com>
2023-09-19 08:54:27 +0700

    Redefined the onSaveInstanceState(Bundle? method

--- app/src/main/java/com/example/geoquize/MainActivity.kt
+++ app/src/main/java/com/example/geoquize/MainActivity.kt
@@ -14,6 +14,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var falseButton:Button
     private lateinit var nextButton:Button
     private lateinit var questionTextView: TextView
+    private var canAnswer = true
+    private var canGoToNext = false
+
+    private  val KEY_INDEX="index"
     private val questionBank=listOf(
         Question(R.string.question_australia,true),
         Question(R.string.question_oceans,true),
@@ -25,16 +29,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState != null) {
+            currentIndex = savedInstanceState.getInt(KEY_INDEX, 0)
+        }
         trueButton=findViewById(R.id.true_button)
         falseButton=findViewById(R.id.false_button)
         nextButton=findViewById(R.id.next_button)
         questionTextView=findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener{view: View->
-           checkAnswer(true)
+            if (canAnswer) {
+                checkAnswer(true)
+                canAnswer = false
+                canGoToNext = true
+                updateButtonState()
+            }
         }
         falseButton.setOnClickListener{view:View ->
-          checkAnswer(false)
+            if (canAnswer) {
+                checkAnswer(false)
+                canAnswer = false
+                canGoToNext = true
+                updateButtonState()
+            }
         }
         nextButton.setOnClickListener{view:View->
             currentIndex=(currentIndex + 1) % questionBank.size
@@ -42,6 +60,16 @@ class MainActivity : AppCompatActivity() {
         }
         updateQuestion()
     }
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putInt(KEY_INDEX, currentIndex)
+    }
+    private fun updateButtonState() {
+        trueButton.isEnabled = canAnswer
+        falseButton.isEnabled = canAnswer
+        nextButton.visibility = if (canGoToNext) View.VISIBLE else View.INVISIBLE
+    }
+
     private fun updateQuestion(){
         val questionTextResId=questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
