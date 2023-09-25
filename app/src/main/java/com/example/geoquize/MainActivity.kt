f635ddc3297065f7839c899a511827c342595c92
Anastasia <aachuchkalova@gmail.com>
2023-09-25 21:56:34 +0700

    Updating a question via Quiz View Model

--- app/src/main/java/com/example/geoquize/MainActivity.kt
+++ app/src/main/java/com/example/geoquize/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.geoquize
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,8 +10,11 @@ import android.view.Gravity
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.bignerdranch.android.geoquiz.QuizViewModel
+import androidx.fragment.app.viewModels
+
 
 private const val TAG = "MainActivity"
 
@@ -21,26 +23,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var falseButton:Button
     private lateinit var nextButton:Button
     private lateinit var questionTextView: TextView
-    private var currentIndex=0;
-
-
-
     private  val KEY_INDEX="index"
-    private val questionBank=listOf(
-        Question(R.string.question_australia,true),
-        Question(R.string.question_oceans,true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true))
+    private val quizViewModel: QuizViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
-        val provider:ViewModelProvider=ViewModelProvider(this)
+        /*val provider:ViewModelProvider=ViewModelProvider(this)
         val quizViewModel=provider.get(QuizViewModel::class.java)
-        Log.d(TAG,"Got a QuizViewModel:$quizViewModel")
+        Log.d(TAG,"Got a QuizViewModel:$quizViewModel")*/
         trueButton=findViewById(R.id.true_button)
         falseButton=findViewById(R.id.false_button)
         nextButton=findViewById(R.id.next_button)
@@ -55,7 +47,7 @@ class MainActivity : AppCompatActivity() {
 
         }
         nextButton.setOnClickListener{view:View->
-            currentIndex=(currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
            updateQuestion()
         }
 
@@ -83,11 +75,11 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG,"onDestroy() called")
     }
     private fun updateQuestion(){
-        val questionTextResId=questionBank[currentIndex].textResId
+        val questionTextResId=quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
     private fun checkAnswer(userAnswer:Boolean){
-        val correctAnswer=questionBank[currentIndex].answer
+        val correctAnswer=quizViewModel.currentQuestionAnswer
         val messageResId= if (userAnswer==correctAnswer){
             R.string.correct_toast
         }
