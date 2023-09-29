db18ad3275786963ebd164ed825a3021b4b1f9a1
Anastasia <aachuchkalova@gmail.com>
2023-09-29 22:24:15 +0700

    Renaming two files.

--- app/src/main/java/com/example/geoquize/CheatActivity.kt
+++ app/src/main/java/com/example/geoquize/CheatActivity.kt
@@ -1,24 +1,14 @@
 package com.example.geoquize
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import android.widget.TextView
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.geoquize.ui.theme.GeoquizeTheme
 
-class ResultActivity : ComponentActivity() {
+class CheatActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        setContentView(R.layout.activity_cheat)
 
         val correctAnswers = intent.getIntExtra("CORRECT_ANSWERS", 0)
 
